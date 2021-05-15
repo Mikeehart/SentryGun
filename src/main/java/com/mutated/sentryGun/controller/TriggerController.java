59ab65e1c09a0ea4cade0157348d725ce6660de7
mike ehart <michaelt.ehart@gmail.com>
2021-05-15 15:46:04 -0700

    added 2nd relay

--- src/main/java/com/mutated/sentryGun/controller/TriggerController.java
+++ src/main/java/com/mutated/sentryGun/controller/TriggerController.java
@@ -7,25 +7,77 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TriggerController {
 
-    private static GpioPinDigitalOutput pin;
+    private static GpioPinDigitalOutput relayPin1;
+    private static GpioPinDigitalOutput relayPin2;
+    private boolean isFiring = false;
 
     @RequestMapping("/")
     public String bootMessage(){
         return "Sentry gun online";
     }
 
-    @RequestMapping("/primerTrigger")
-    public String primerTrigger(){
+    private String primerTrigger(){
 
-        if(pin == null) {
+        if(relayPin1 == null) {
 
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "primer", PinState.LOW);
+            relayPin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "primer relay", PinState.LOW);
         }
 
-        pin.toggle();
+        relayPin1.toggle();
 
-        return "Sentry arming...";
+        return "primerTrigger() called";
     }
 
+    private String fireTrigger(){
+
+        if(relayPin2 == null) {
+
+            GpioController gpio = GpioFactory.getInstance();
+            relayPin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "fire relay", PinState.LOW);
+        }
+
+        relayPin2.toggle();
+
+            return "fireTrigger() called";
+    }
+
+    private String fire() throws InterruptedException {
+
+        primerTrigger();
+        Thread.sleep(2000);
+        fireTrigger();
+        isFiring = true;
+
+        return "Engaging target";
+
+    }
+
+    private String ceaseFire() throws InterruptedException {
+
+        fireTrigger();
+        Thread.sleep(1000);
+        primerTrigger();
+        isFiring = false;
+
+        return "Target neutralized";
+    }
+
+    @RequestMapping("/toggleTrigger")
+    public String toggleTrigger(){
+
+        String message = "exception caught";
+        try {
+                if(!isFiring)
+                    message = fire();
+                else
+                    message = ceaseFire();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+
+        return message;
+    }
 }
